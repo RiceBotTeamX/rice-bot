9f8797266a52b3b7643600936a141f0a7bf8a235
prashanth314 <43122131+prashanth314@users.noreply.github.com>
2018-09-20 14:56:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -415,7 +415,7 @@ def get_response_text(message):
                     if is_open(servery, dining_data):
                         menu_text = print_menu(servery, dining_data)
                         if menu_text:
-                            response_message += menu_text + " today.\n \n"
+                            response_message += menu_text + " today. " + EMOJIS[random.choice(list(EMOJIS))] + "\n \n"
                         else:
                             response_message += "We don't know the menu for " + servery.capitalize() + " right now.\n \n"
 
@@ -439,7 +439,7 @@ def get_response_text(message):
             if wit_traits["bye"]:
                 response_message += "You can chat with me whenever!\n"
 
-            response_message += "\n" + + EMOJIS[random.choice(list(EMOJIS))]
+            response_message += "\n"
 
             # General statement regarding eating
             if eating and not wit_traits["bye"]:
